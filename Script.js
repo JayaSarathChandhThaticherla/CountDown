4e3b2bf0129da4fd954fc642301d91d278848b68
sarathchandh <sarathchandh1428@gmail.com>
2025-07-03 09:58:20 -0700

    First Commit

---
+++ Script.js
@@ -0,0 +1,18 @@
+const countdown = () => {
+    const endDate = new Date("Jul 12, 2025 19:00:00").getTime();
+    const now = new Date().getTime();
+    const timeLeft = endDate - now;
+  
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+    const seconds = Math.floor((timeLeft / 1000) % 60);
+  
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+  };
+  
+  setInterval(countdown, 1000);
+  
